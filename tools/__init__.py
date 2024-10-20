2e186fabb73a0171bb82f51c5fa1e9cbec4bb329
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-10-19 19:20:29 -0600

    ✨ Add Gmail toolkit and agent toolkit functions

---
+++ tools/__init__.py
@@ -0,0 +1,28 @@
+from .gmail import get_gmail_toolkit
+from typing import List, Any
+from langchain_google_community.gmail.utils import (
+    build_resource_service,
+    get_gmail_credentials,
+)
+
+
+__all__ = ["get_agent_toolkit"]
+
+
+SCOPES = ["https://mail.google.com/", "https://www.googleapis.com/auth/calendar"]
+
+def build_service(scopes: List[str], 
+                  token_file: str = "./secrets/token.json", 
+                  client_secrets_file: str = "./secrets/credentials.json") -> Any:
+    
+    credentials = get_gmail_credentials(
+        token_file=token_file, scopes=scopes, client_secrets_file=client_secrets_file
+    )
+    return build_resource_service(credentials=credentials)
+
+def get_agent_toolkit() -> List[Any]:
+    api_resource = build_service(scopes=SCOPES)
+    return get_gmail_toolkit(api_resource=api_resource)
+
+
+
---
+++ tools/gmail.py
@@ -0,0 +1,7 @@
+from typing import Any, List
+from langchain_google_community import GmailToolkit
+
+
+def get_gmail_toolkit(api_resource: Any) -> List[Any]:
+    toolkit = GmailToolkit(api_resource=api_resource)
+    return toolkit.get_tools()
