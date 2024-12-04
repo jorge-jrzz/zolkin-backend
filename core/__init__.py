71cf51a97eedf34d00ec8f37b6d15bd36467ad63
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-12-04 13:39:08 -0600

    ✨ Add MilvusStorage class for managing vector storage and retrieval

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+from .google_auth import get_google_creds
+from .files_strategy import manage_files
+from .ocr import LoadFile
+from .agent.tools import MilvusStorage
+from .agent.memory import RedisSaver
+from .agent import ZolkinAgent
+
+__all__ = ["get_google_creds", "manage_files", "LoadFile", "ZolkinAgent", "MilvusStorage", "RedisSaver"]
