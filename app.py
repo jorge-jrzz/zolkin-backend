50c3e39774f9aefd6a6a77cdd3f8a470c85dd46d
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-12-10 00:59:43 -0600

    ♻️ Refactor app.py to enhance Google OAuth integration, file upload handling, and user session management

--- app.py
+++ app.py
@@ -1,41 +1,50 @@
+"""Zolkin API."""
+
 import os
 from typing import List
 
-from dotenv import load_dotenv
-from flask import Flask, url_for, redirect, session, jsonify, request
-from authlib.integrations.flask_client import OAuth
-from authlib.common.security import generate_token
-from werkzeug.utils import secure_filename
+from redis import Redis
 from flask_cors import CORS
+from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 from langchain_core.messages import HumanMessage
+from authlib.common.security import generate_token
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, url_for, redirect, session, jsonify, request
 
 from core import get_google_creds, manage_files, LoadFile, ZolkinAgent, MilvusStorage, RedisSaver
 
-from redis import Redis
-
 
 load_dotenv()
-SCOPES = ["https://mail.google.com/", "https://www.googleapis.com/auth/calendar", "openid", "email", "profile"]
+SCOPES = [
+    "https://mail.google.com/", 
+    "https://www.googleapis.com/auth/calendar", 
+    "openid", 
+    "email", 
+    "profile"
+]
 CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = [
+    'pdf', 'png', 'jpg', 'jpeg', 'ppt', 'pptx', 'doc', 'docx'
+]
 
 app = Flask(__name__)
-app.secret_key = os.urandom(12)
-oauth = OAuth(app)
-CORS(app)
-
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = ['txt', 'html', 'md', 'java', 'py', 'c', 'cpp', 'js', 'pdf', 
-                      'png', 'jpg', 'jpeg', 'ppt', 'pptx', 'doc', 'docx']
+app.secret_key = os.getenv("SECRET_KEY")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+oauth = OAuth(app)
+CORS(app, resources={r"/*": {"origins": os.getenv('FRONTEND_URL')}}, supports_credentials=True)
 
+# Diccionarios globales para almacenar agentes por usuario (clave: email)
+active_agents = {}   # almacenará el agente ZolkinAgent por email
+active_zolkin_agents = {}  # almacenará la instancia creada con memory y demás
+active_user_data = {} # almacenar datos de usuario por email
+active_milvus_storage = {} # milvus storage por email
+
+redis_conn = Redis(host="redis", port=6379, db=0)
+# Instancia global de MilvusStorage (colección base)
 milvus_conn = MilvusStorage(collection_name="zolkin_collection")
-milvus_storage = None
-redis_conn = Redis(host="localhost", port=6379, db=0)
 memory = RedisSaver(redis_conn)
-agent: ZolkinAgent = None
-zolkin_agent = None
-user_info_data = None
-creds = None
 
 
 def allowed_file(filename: str, extensions: List[str]) -> bool:
@@ -49,7 +58,6 @@ def allowed_file(filename: str, extensions: List[str]) -> bool:
     Returns:
         bool: True if the file format is allowed, False otherwise.
     """
-
     # Primero crea una lista con el nombre del archivo y la extensión
     # Selecciona el segundo elemento de la lista, que es la extensión y la convierte a minúsculas
     # Comprueba si la extensión está en la lista de extensiones permitidas
@@ -59,12 +67,23 @@ def allowed_file(filename: str, extensions: List[str]) -> bool:
 
 @app.route('/')
 def index():
+    """Check if the server is running."""
     return "Hello World"
 
 
 @app.post('/upload_file/')
 def upload_file():
-    global user_info_data, milvus_conn, milvus_storage, agent
+    """Endpoint to upload a file to the server."""
+    # Obtenemos el email del usuario actual
+    if 'user' not in session or 'email' not in session['user']:
+        return jsonify({"error": "User not authenticated"}), 401
+    user_email = session['user']['email']
+    # Verificamos si existe un agente asociado a este usuario
+    if user_email not in active_agents:
+        return jsonify({"error": "Agent not initialized for this user"}), 400
+    agent = active_agents[user_email]
+    milvus_storage = active_milvus_storage[user_email]
+    user_info_data = active_user_data[user_email]
     if 'file' not in request.files:
         return jsonify({"error": "No file has been sent: 'file'"}), 400
     elif request.form.get("filename") is None:
@@ -75,14 +94,16 @@ def upload_file():
     if file and allowed_file(file.filename, ALLOWED_EXTENSIONS):
         new_filename = f"{request.form.get('filename')}.{file.filename.rsplit('.', 1)[1].lower()}"
         filename = secure_filename(new_filename)
+        os.makedirs(os.path.dirname(UPLOAD_FOLDER), exist_ok=True)
         path_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(path_file)
         # Empieza RAG
+        os.makedirs(os.path.dirname("pdfs/"), exist_ok=True)
         manage_files(path_file, dst_path=f"./pdfs/{filename}")
         LoadFile.ocr_pdf(f"./pdfs/{filename}")
         file_content = LoadFile.load_file(
-            file_path = f"./pdfs/{filename}", 
-            namespace = user_info_data["email"]
+            file_path=f"./pdfs/{filename}", 
+            namespace=user_info_data["email"]
         )
         milvus_conn.upsert_files(milvus_storage, file_content)
         agent.update_rag_description("zolkin_collection")
@@ -93,6 +114,7 @@ def upload_file():
 
 @app.route('/google/')
 def google():
+    """Endpoint to start the Google OAuth2 flow."""
     oauth.register(
         name='google',
         client_id=os.getenv('GOOGLE_CLIENT_ID'),
@@ -105,16 +127,17 @@ def google():
         }
     )
     redirect_uri = url_for('google_auth', _external=True)
-    print(redirect_uri)
     session['nonce'] = generate_token()
     return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
 
 
 @app.route('/google/auth/')
 def google_auth():
-    global creds, user_info_data, milvus_storage, milvus_conn, agent, zolkin_agent, memory
+    """Endpoint to authenticate the user with Google."""
     token = oauth.google.authorize_access_token()
-    creds = get_google_creds("./secrets/token.json", token, SCOPES)
+    token_file= f"./secrets/{token["userinfo"]["email"].rsplit('@', 1)[0].lower()}.json"
+    os.makedirs(os.path.dirname("secrets/"), exist_ok=True)
+    creds = get_google_creds(token_file, token, SCOPES)
     user_info_data = {
         "name": token["userinfo"]["name"], 
         "email": token["userinfo"]["email"], 
@@ -122,8 +145,11 @@ def google_auth():
     }
     user = oauth.google.parse_id_token(token, nonce=session['nonce'])
     session['user'] = user
-    # Empieza Agente
+    # Guardamos la info del usuario en el diccionario global
+    active_user_data[user_info_data["email"]] = user_info_data
+    # Empieza agente para este usuario
     milvus_storage = milvus_conn.use_collection()
+    active_milvus_storage[user_info_data["email"]] = milvus_storage
     agent = ZolkinAgent(
         google_creds=creds,
         milvus_conn=milvus_conn,
@@ -131,26 +157,40 @@ def google_auth():
         partition_key_field=user_info_data["email"]
     ).init_tools()
     zolkin_agent = agent.create_agent(memory)
-    return redirect('http://localhost:5173/chat')
+    # Guardamos el agente en el diccionario global
+    active_agents[user_info_data["email"]] = agent
+    active_zolkin_agents[user_info_data["email"]] = zolkin_agent
+    return redirect(f"{os.getenv('FRONTEND_URL')}/chat")
 
 
 @app.route('/user_info/')
 def user_info():
-    return jsonify(user_info_data), 200
+    """Endpoint to get the user info."""
+    if 'user' not in session or 'email' not in session['user']:
+        return jsonify({"error": "User not authenticated"}), 401
+    user_email = session['user']['email']
+    if user_email in active_user_data:
+        return jsonify(active_user_data[user_email]), 200
+    else:
+        return jsonify({"error": "No user data found"}), 404
 
 
 @app.route('/chat/')
 def chat():
-    global zolkin_agent
-    if zolkin_agent is None:
+    """Endpoint to interact with the Zolkin Agent."""
+    # Obtenemos el email del usuario actual
+    if 'user' not in session or 'email' not in session['user']:
+        return jsonify({"error": "User not authenticated"}), 401
+    user_email = session['user']['email']
+    if user_email not in active_zolkin_agents:
         return jsonify({"error": "Agent not initialized"}), 400
-    elif request.args.get("prompt") is None or request.args.get("thread_id") is None:
-        return jsonify({"error": "No prompt has been sent"}), 401
+    zolkin_agent = active_zolkin_agents[user_email]
+    if request.args.get("prompt") is None or request.args.get("thread_id") is None:
+        return jsonify({"error": "No prompt or thread_id has been sent"}), 401
     config = {"configurable": {"thread_id": request.args.get("thread_id")}}
-    response = zolkin_agent.invoke(
-        {
+    response = zolkin_agent.invoke({
             "messages": [HumanMessage(content=request.args.get("prompt"))]
-        }, 
+        },
         config
     )
     return jsonify({"response": response["messages"][-1].content}), 200
