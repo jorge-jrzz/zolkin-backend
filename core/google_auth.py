7bec6335d43d4c5bc97274871171a70592299331
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2025-01-01 22:08:58 -0600

    💡 Enhance Google authentication functionality with improved docstrings and type hints

--- core/google_auth.py
+++ core/google_auth.py
@@ -1,3 +1,7 @@
+"""
+Functionalities for Google authentication and token management.
+"""
+
 import os
 from typing import Dict, List, Any
 
@@ -5,7 +9,19 @@ from datetime import datetime, timedelta, UTC
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 
-def get_google_creds(token_file, token_data: Dict[str, Any], scopes: List[str]) -> Credentials:
+
+def get_google_creds(token_file: str, token_data: Dict[str, Any], scopes: List[str]) -> Credentials:
+    """
+    Get Google credentials from token data and generate a token file.
+
+    Args:
+        token_file (str): The path to the token file.
+        token_data (Dict[str, Any]): The token data to use for authentication.
+        scopes (List[str]): The scopes to use for authentication.
+
+    Returns:
+        Credentials: The Google credentials object.
+    """
     creds = None
     if os.path.exists(token_file):
         creds = Credentials.from_authorized_user_file(token_file, scopes)
@@ -13,17 +29,18 @@ def get_google_creds(token_file, token_data: Dict[str, Any], scopes: List[str]) 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-        else:         
+        else:
             creds = Credentials(
-                token = token_data['access_token'],
-                refresh_token = token_data['refresh_token'],
-                client_id = os.getenv('GOOGLE_CLIENT_ID'),
-                client_secret = os.getenv('GOOGLE_CLIENT_SECRET'),
-                token_uri = "https://oauth2.googleapis.com/token",
-                scopes = scopes,
-                expiry = datetime.now(UTC) + timedelta(seconds=token_data['expires_in']),
+                token=token_data['access_token'],
+                refresh_token=token_data['refresh_token'],
+                client_id=os.getenv('GOOGLE_CLIENT_ID'),
+                client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
+                token_uri="https://oauth2.googleapis.com/token",
+                scopes=scopes,
+                expiry=datetime.now(
+                    UTC) + timedelta(seconds=token_data['expires_in']
+                ),
             )
         with open(token_file, "w", encoding="utf-8") as file:
             file.write(creds.to_json())
     return creds
-            
