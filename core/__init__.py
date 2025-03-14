e78eec981170775cad590277e95fc74bcbf03f35
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2025-03-14 15:10:36 -0600

    ✨ Add references for core components

--- core/__init__.py
+++ core/__init__.py
@@ -1,17 +1,38 @@
 """Core module for Zolkin project."""
-
-from .google_auth import get_google_creds
-from .files_strategy import manage_files
-from .ocr import LoadFile
-from .agent.tools import MilvusStorage
 from .agent.memory import RedisSaver
-from .agent import ZolkinAgent
+from .processing.ocr import LoadFile
+from .agent.tools import MilvusStorage
+from .auth.google_auth import get_google_creds
+from .auth.user_manager import users_cache, get_user, set_user
+from .processing.files_strategy import manage_files
+from .agent import (
+    ZolkinAgent,
+    agent_cache,
+    get_agent,
+    set_agent,
+    remove_agent,
+    zolkin_cache,
+    set_zolkin,
+    remove_zolkin,
+    get_zolkin
+)
 
 __all__ = [
-    "get_google_creds", 
-    "manage_files", 
-    "LoadFile", 
-    "ZolkinAgent", 
-    "MilvusStorage", 
-    "RedisSaver"
+    "get_google_creds",
+    "manage_files",
+    "LoadFile",
+    "ZolkinAgent",
+    "MilvusStorage",
+    "RedisSaver",
+    "agent_cache",
+    "get_agent",
+    "set_agent",
+    "remove_agent",
+    "zolkin_cache",
+    "set_zolkin",
+    "remove_zolkin",
+    "get_zolkin", 
+    "users_cache",
+    "get_user",
+    "set_user"
 ]
