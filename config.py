8a07f8964e1e2d7361511ecde620d645c8eeb73f
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2025-03-14 15:15:19 -0600

    🔧 Update config flask app

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-BASE_DIR = "./app/"
-UPLOAD_FOLDER = BASE_DIR + "files/uploads/"
-
+"""Configuration for development environment"""
 class DevelopmentConfig():
     SECRET_KEY = 'application_by_yorch'
     DEBUG = True
     HOST = '0.0.0.0'
     PORT = 5002
-    UPLOAD_FOLDER = UPLOAD_FOLDER
